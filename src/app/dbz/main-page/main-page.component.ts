10997fe7f5980058e0c5e1aa405779bd315ea507
Miguel Luque <miguel.luque@huelladigital.tvm>
2021-03-14 15:58:21 -0500

    inputs Angular leccion 60

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,29 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-
-interface Personaje{
- nombre: string;
- poder: number;
-}
+import { Component } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.scss']
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent {
  
   // personajes:string[] = ['Krilin-700','Goku-1500','Vegeta-800'];
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+ personajes:Personaje []= [
+   {
+      nombre:'Goku',
+      poder: 15000
+   },
+   {  
+     nombre:'Vegeta',   
+     poder: 7500           
+   }
+];
+  
   nuevo: Personaje = {
-    nombre: 'Trunks',
-    poder: 14000
+    nombre: '',
+    poder: 0
   }
   agregar(){
-    console.log(this.nuevo);
+    if ( this.nuevo.nombre.trim().length === 0 ){
+      return;
+    }
+     this.personajes.push(this.nuevo); //Forma de agregar un valor a un array
+     this.nuevo = {
+       nombre:'',
+       poder: 0
+     };
   }
 
   /*agregar( event:any){ metodo clasico con Javascypt
---
+++ src/app/dbz/personajes/personajes.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-personajes',
+  templateUrl: './personajes.component.html',
+  styleUrls: ['./personajes.component.scss']
+})
+export class PersonajesComponent  {
+  /*El @input sirve para importar propiedades del componente 
+  padre y en este caso para importar la propiedad personajes 
+  del padre al array en el componente hijo, a la hora de llamar 
+  el componente en el padre se usa [data]="personajes" 
+  (linea 5 main-page.component.hml) en la etiqueta y se le 
+  llama cómo data porque así se l nombra en los parentesis del @input,
+  si quitara esto, sedeberia llamar en el html la propiedad por el nombre 
+  original [personajes]="personajes"*/
+  @Input('data') personajes:any[] = []; 
+}
